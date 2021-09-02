524403aaf9fba032974532f3d3f9b491f1272dcc
lcseferreira <lcseferreira@hotmail.com>
2021-09-02 17:03:44 -0300

    Renamed project

--- src/main/java/com/lucasferreira/jparepository/JPArepositoryApplication.java
+++ src/main/java/com/lucasferreira/jparepository/JPArepositoryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JparepositoryApplication {
+public class JPArepositoryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JparepositoryApplication.class, args);
+		SpringApplication.run(JPArepositoryApplication.class, args);
 	}
 
 }
